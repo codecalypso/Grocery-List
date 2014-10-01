3f90c35b4149964e0a4613160dbec9bfec787f5e
Stacy Mullins <smullins10@gmail.com>
2014-09-30 21:01:50 -0500

    Modify style issues

--- shopping_list.rb
+++ shopping_list.rb
@@ -1,35 +1,34 @@
 def create_list
-print "What is the list name "
-name = gets.chomp
-hash = {"name" =>name, "items" =>Array.new}
-return hash
+  print "What is the list name "
+  name = gets.chomp
+  hash = { "name" => name, "items" => Array.new }
+  hash
 end
-
 
 def add_list_item
-	print "What is the item called?"
-	item_name = gets.chomp
+  print "What is the item called?"
+  item_name = gets.chomp
 
-	print "how much "
-	quantity = gets.chomp.to_i  
-	hash = {"name" => item_name, "quantity" => quantity}
-	return hash
+  print "how much "
+  quantity = gets.chomp.to_i
+  hash = {"name" => item_name, "quantity" => quantity}
+  hash
 end
+
 def print_separator(character="-")
-	puts character * 80
+  puts character * 80
 end
 
 def print_list(list)
-	puts "List: #{list["name"]}"
-	print_separator
-
-	list["items"].each do |item|
-		puts "\tItem: " + item['name'] + "\t\t\t" +
-		     "Quantity: " + item['quantity'].to_s
-		
-	end
-	print_separator
+  puts "List: #{list["name"]}"
+  print_separator
+  list["items"].each do |item|
+  puts "\tItem: " + item['name'] + "\t\t\t" +
+         "Quantity: " + item['quantity'].to_s
+           end
+  print_separator
 end
+
 list = create_list()
 
 puts "Great! Add some items to your list"
