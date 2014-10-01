b7d960b1e7e6392b2d2a05ab783aa3906ae4d8d5
Stacy Mullins <smullins10@gmail.com>
2014-09-30 20:47:56 -0500

    Complete shopping list tut

--- shopping_list.rb
+++ shopping_list.rb
@@ -15,20 +15,26 @@ def add_list_item
 	hash = {"name" => item_name, "quantity" => quantity}
 	return hash
 end
+def print_separator(character="-")
+	puts character * 80
+end
 
 def print_list(list)
 	puts "List: #{list["name"]}"
-	puts "-----"
+	print_separator
 
 	list["items"].each do |item|
-		puts "Item: " + item['name']
-		puts "Quantity: " + item['quantity'].to_s
-		puts "-----"
+		puts "\tItem: " + item['name'] + "\t\t\t" +
+		     "Quantity: " + item['quantity'].to_s
+		
 	end
+	print_separator
 end
 list = create_list()
-puts list.inspect
-list["items"].push(add_list_item())
 
-puts list.inspect
+puts "Great! Add some items to your list"
+list["items"].push(add_list_item())
+list["items"].push(add_list_item())
+list["items"].push(add_list_item())
+puts "Here's your list: \n"
 print_list(list)
